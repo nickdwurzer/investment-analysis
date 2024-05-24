86b4566097c4e2cbc46b15462e6cef5da5f4b619
Nicholas Wurzer <nwurzer@Nicks-MacBook-Pro.local>
2024-05-23 17:37:14 -0700

    Catch Errors, Add TODOS

--- db_update.py
+++ db_update.py
@@ -3,15 +3,20 @@ import yahoo_fin.stock_info as si
 import pandas as pd
 from datetime import datetime as dt
 import sqlite3 as sql3
+import requests
 
 #retuns the last trading day in the database for a company.
 def get_last_trade_day(database, ticker):
     conn = sql3.connect(database)
     cursor = conn.cursor()
-    result = cursor.execute('SELECT "index" FROM ' + ticker + ' ORDER BY "index" DESC LIMIT 1')
-    date = result.fetchall()[0][0]
-    conn.close()
-    return date
+    try:
+        result = cursor.execute('SELECT "index" FROM ' + ticker + ' ORDER BY "index" DESC LIMIT 1')
+        date = result.fetchall()[0][0]
+        conn.close()
+        return date
+    except sql3.OperationalError as e:
+        print(e)
+        return None
 
 def update_all(database):
     dow_tickers = si.tickers_dow()
@@ -31,21 +36,31 @@ def update_all(database):
 
 def update_table(database, ticker):
     last_trade_day = get_last_trade_day("historical_price.db", ticker)
-    # Format date and add one day so that there is no overlap
-    last_trade_day_si = last_trade_day[:4]+"/"+last_trade_day[5:7]+"/"+str(int(last_trade_day[8:10])+1)
-    last_trade_day = dt.fromisoformat(str(last_trade_day))
-    try:
-        data = si.get_data(ticker, start_date=last_trade_day_si)
-        retrieved_date = str(data.iloc[0].name)
-        retrieved_date = dt.fromisoformat(retrieved_date)
-        if retrieved_date > last_trade_day:
-            conn = sql3.connect(database)
-            data.to_sql(ticker, conn, if_exists='append')
-            conn.close()
-        else:
-            print("Incorrect start date of reteived data.")
-    except AssertionError:
-        print("Error retrieving data for ticker " + ticker)
+    #TODO if table doesn't exist yet, but data can be retrieved, make a new table.
+    if(last_trade_day):
+        # Format date and add one day so that there is no overlap
+        #TODO change to datetime and increase by one day
+        last_trade_day_si = last_trade_day[:4]+"/"+last_trade_day[5:7]+"/"+str(int(last_trade_day[8:10])+1)
+        last_trade_day = dt.fromisoformat(str(last_trade_day))
+        try:
+            data = si.get_data(ticker, start_date=last_trade_day_si)
+            retrieved_date = str(data.iloc[0].name)
+            retrieved_date = dt.fromisoformat(retrieved_date)
+            if retrieved_date > last_trade_day:
+                conn = sql3.connect(database)
+                data.to_sql(ticker, conn, if_exists='append')
+                conn.close()
+            else:
+                print("Incorrect start date of reteived data.")
+        except AssertionError:
+            #TODO give more helpful message
+            print("Error retrieving data for ticker " + ticker)
+        except KeyError:
+            #TODO give more helpful message
+            print("Error retrieving data for ticker " + ticker)
+        except requests.exceptions.ChunkedEncodingError:
+            #TODO give more helpful message
+            print("Error retrieving data for ticker " + ticker)
 
 #remember that a comapanies very first trading day is the first entity in the table
 def main():
